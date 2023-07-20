62b46a6a744c3d9df3dc3435716ececc2c1ef034
ClydeFrog04 <egan.randal@gmail.com>
2023-07-20 08:56:32 -0600

    testing adding router

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
 /**
  * @author Randi Egan
  */
-// @ts-ignore
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 import {RootProvider} from "./contexts/RootProvider.tsx";
+import {BrowserRouter as Router} from "react-router-dom";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = ReactDOMClient.createRoot(container);
 
 root.render(
-    <RootProvider>
-        <App/>
-    </RootProvider>
+    <Router>
+        <RootProvider>
+            <App/>
+        </RootProvider>
+    </Router>
 );
